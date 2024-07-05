57b975c7e9517080edad28292e342f4dad270532
Shreya Saha <132643578+ShreyaSaha080403@users.noreply.github.com>
2024-07-05 11:13:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Rotate Array/rotate-array.java
+++ Difficulty: Easy/Rotate Array/rotate-array.java
@@ -65,5 +65,24 @@ class Solution {
             arr[i]=temp[i-n+d];
         }
         
+        
+//         public static void Rotateeletoleft(int[] arr, int n, int k) {
+//     // Reverse first k elements
+//           Reverse(arr, 0, k - 1);
+//     // Reverse last n-k elements
+//     Reverse(arr, k , n - 1);
+//     // Reverse whole array
+//     Reverse(arr, 0, n - 1);
+//   }
+//         public static void Reverse(int[] arr, int start, int end) {
+//     while (start <= end) {
+//       int temp = arr[start];
+//       arr[start] = arr[end];
+//       arr[end] = temp;
+//       start++;
+//       end--;
+//     }
+//   }
+        
     }
 }
