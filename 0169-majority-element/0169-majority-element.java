75f48204b8b0dfb65795856f8885306bf729a0ed
Shreya Saha <132643578+ShreyaSaha080403@users.noreply.github.com>
2024-08-08 15:30:13 +0530

    Time: 1 ms (99.79%), Space: 52.7 MB (64.5%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -2,21 +2,33 @@ class Solution {
     public int majorityElement(int[] nums) {
         //Better Solution
 
-        HashMap<Integer,Integer> mp=new HashMap<>();
-        int n=nums.length;
-        for(int i=0;i<nums.length;i++){
-            int val=mp.getOrDefault(nums[i],0);
-            mp.put(nums[i],val+1);
-        }
-        for(Map.Entry<Integer,Integer> e: mp.entrySet()){
-            if(e.getValue()>n/2){
-                return e.getKey();
-            }
-        }
-        return -1;
+        // HashMap<Integer,Integer> mp=new HashMap<>();
+        // int n=nums.length;
+        // for(int i=0;i<nums.length;i++){
+        //     int val=mp.getOrDefault(nums[i],0);
+        //     mp.put(nums[i],val+1);
+        // }
+        // for(Map.Entry<Integer,Integer> e: mp.entrySet()){
+        //     if(e.getValue()>n/2){
+        //         return e.getKey();
+        //     }
+        // }
+        // return -1;
 
-        //optimal
+        //optimal //moores voing algo
         
-
+         int ele=0;
+         int cnt=0;
+         for(int i=0;i<nums.length;i++){
+            if(cnt==0){
+                cnt=1;
+                ele=nums[i];
+            }
+            else if(ele==nums[i]){
+                cnt++;
+            }
+            else cnt--;
+         }
+         return ele;
     }
 }
