ee310048ca78366db359c29b69c16365aee153ad
Shreya Saha <132643578+ShreyaSaha080403@users.noreply.github.com>
2024-09-13 00:19:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Stack using Linked List/implement-stack-using-linked-list.java
@@ -0,0 +1,96 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class StackNode {
+    int data;
+    StackNode next;
+
+    StackNode(int a) {
+        data = a;
+        next = null;
+    }
+}
+
+class GfG {
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        scanner.nextLine(); // Flush the newline character left by nextInt
+        while (T-- > 0) {
+            MyStack sq = new MyStack();
+            String line = scanner.nextLine();
+            Scanner ss = new Scanner(line);
+            List<Integer> nums = new ArrayList<>();
+            while (ss.hasNextInt()) {
+                int num = ss.nextInt();
+                nums.add(num);
+            }
+            int n = nums.size();
+            int i = 0;
+            while (i < n) {
+                int QueryType = nums.get(i++);
+                if (QueryType == 1) {
+                    int a = nums.get(i++);
+                    sq.push(a);
+                } else if (QueryType == 2) {
+                    System.out.print(sq.pop() + " ");
+                }
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class MyStack {
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }
+    StackNode top;
+
+    // Function to push an integer into the stack.
+    void push(int a) {
+        // Add your code here
+        StackNode newnode=new StackNode(a);
+        if(top==null){
+            top=newnode;
+            return;
+        }
+        StackNode temp=top;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newnode;
+    }
+
+    // Function to remove an item from top of the stack.
+    int pop() {
+        // Add your code here
+        if(top==null){
+            return -1;
+        }
+         if(top.next==null){
+            int x=top.data;
+            top=null;
+            return x;
+        }
+        StackNode prev=top;
+        StackNode curr=top.next;
+        while(curr.next!=null){
+            curr=curr.next;
+            prev=prev.next;
+        }
+        int x=curr.data;
+        prev.next=prev.next.next;
+        return x;
+    }
+}
