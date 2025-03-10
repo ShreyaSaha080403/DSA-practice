7b7484d92f8b262f3fdfc95e0327525da5bccd7b
Shreya Saha <132643578+ShreyaSaha080403@users.noreply.github.com>
2025-03-10 21:47:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.java
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -9,11 +9,10 @@ class GFG {
         int T = Integer.parseInt(br.readLine().trim());
         while (T-- > 0) {
             String s1 = br.readLine();
-            String[] S = s1.split(" ");
-            String s = S[0];
-            String t = S[1];
+            String s2 = br.readLine();
+
             Solution ob = new Solution();
-            int ans = ob.editDistance(s, t);
+            int ans = ob.editDistance(s1, s2);
             System.out.println(ans);
 
             System.out.println("~");
@@ -27,27 +26,33 @@ class GFG {
 class Solution {
     public int editDistance(String s1, String s2) {
         // Code here
-        int m = s1.length();
-        int n = s2.length();
-        int[][] dp = new int[m+1][n+1];
+         // Code here
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n+1][m+1];
         
-        for(int i=0;i<=m;i++)
-            dp[i][0] = i;
-       
-        for(int j=0;j<=n;j++)
+         for(int i=0;i<=n;i++)
+         {
+          dp[i][0] = i;
+         }
+         for(int j=0;j<=m;j++)
+         {
             dp[0][j] = j;
-        
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-               
+         }
+         for(int i=1;i<=n;i++)
+         {
+             for(int j=1;j<=m;j++)
+             {
                 if(s1.charAt(i-1)==s2.charAt(j-1))
-                    dp[i][j] = dp[i-1][j-1];
-               
-                else 
-                    dp[i][j] = Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1]))+1;
-            }
-        }
-      
-        return dp[m][n];
+                {
+                     dp[i][j] = dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = 1+Math.min(dp[i-1][j],Math.min(dp[i][j-1],dp[i-1][j-1])); 
+                }
+             }
+         }
+        return dp[n][m];
     }
 }
